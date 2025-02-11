4227deb14baef78dddccd1c5b8dce54a7de1b1e7
root <root@vmi2422948.contaboserver.net>
2025-02-11 07:14:56 -0600

    optmized the code to remove errors on the code for the Navbar component

--- client/frontend/src/Components/Includes/Navbar.tsx
+++ client/frontend/src/Components/Includes/Navbar.tsx
@@ -24,10 +24,12 @@ export function NavbarDefault() {
             <Typography
                 as="li"
                 variant="small"
-                color="blue-gray"
                 className="flex items-center gap-x-2 p-1 text-lg font-medium"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
                 <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
                 </svg>
@@ -40,10 +42,12 @@ export function NavbarDefault() {
             <Typography
                 as="li"
                 variant="small"
-                color="blue-gray"
                 className="flex items-center gap-x-2 p-1 text-lg font-medium"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
                 </svg>
                 <a href="#" className="flex items-center">
@@ -55,10 +59,12 @@ export function NavbarDefault() {
             <Typography
                 as="li"
                 variant="small"
-                color="blue-gray"
                 className="flex items-center gap-x-2 p-1 text-lg font-medium"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path d="M19.5 21a3 3 0 0 0 3-3v-4.5a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h15ZM1.5 10.146V6a3 3 0 0 1 3-3h5.379a2.25 2.25 0 0 1 1.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 0 1 3 3v1.146A4.483 4.483 0 0 0 19.5 9h-15a4.483 4.483 0 0 0-3 1.146Z" />
                 </svg>
 
@@ -71,10 +77,12 @@ export function NavbarDefault() {
             <Typography
                 as="li"
                 variant="small"
-                color="blue-gray"
                 className="flex items-center gap-x-2 p-1 text-lg font-medium"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
                 <path fill-rule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clip-rule="evenodd" />
                 </svg>
@@ -88,9 +96,14 @@ export function NavbarDefault() {
   );
  
   return (
-    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
+    <Navbar
+      className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4"
+      placeholder=""
+      onPointerEnterCapture={() => {}}
+      onPointerLeaveCapture={() => {}}
+    >
       <div className="container mx-auto flex items-center justify-between text-blue-600">
-        <a href="/"><img src="/Logo/create-with-eric.png" alt="Developer and Designer - Create with Eric" loading="lazy" width={100} /></a>
+        <Link to="/"><img src="/Logo/create-with-eric.png" alt="Developer and Designer - Create with Eric" loading="lazy" width={100} /></Link>
         {/* <Typography
           as="a"
           href="#"
@@ -106,6 +119,9 @@ export function NavbarDefault() {
                 size="md"
                 color="blue"
                 className="hidden lg:inline-block"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
             >
                 <span>Let's Chat</span>
             </Button>
@@ -116,6 +132,9 @@ export function NavbarDefault() {
           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
           ripple={false}
           onClick={() => setOpenNav(!openNav)}
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
         >
           {openNav ? (
             <svg
@@ -154,7 +173,16 @@ export function NavbarDefault() {
           {navList}
           <Link to="/contact/">
             <div className="flex items-center gap-x-1">
-                <Button fullWidth variant="gradient" size="sm" className="" color="blue">
+                <Button 
+                fullWidth 
+                variant="gradient"  
+                className="" 
+                color="blue"
+                size="md"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+                >
                 <span>Let's Chat</span>
                 </Button>
             </div>
